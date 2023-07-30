7c908b1706c06ed4c0a12bd18a0c83bb1a566859
jose <hernandezjos1121@gmail.com>
2023-07-30 16:44:45 -0400

    poop

--- Components/TodoList.js
+++ Components/TodoList.js
@@ -1,10 +1,42 @@
+import { onSnapshot, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import Link from "next/link";
+
 export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "todos"), (snapchat) => {
+      console.log(snapchat);
+      setTodos(
+        snapchat.docs.map((doc) => {
+          return {
+            id: doc.id,
+            title: doc.data().title,
+            detail: doc.data().detail,
+          };
+        })
+      );
+    });
+    return unsubscribe;
+  }, []);
   return (
     <>
       <div>
         <h1>Todos</h1>
         <ul className="todoList">
-          <li className="listItem">Todo 1</li>
+          {todos.map((todo) => {
+            return (
+              <Link key={todo.id} href={"/" + todo.id}>
+              <li  className="listItem">
+                {todo.title}
+              </li>
+            </Link>
+            )
+            
+          })}
+
           <li className="listItem">Todo 2</li>
           <li className="listItem">Todo 3</li>
         </ul>
